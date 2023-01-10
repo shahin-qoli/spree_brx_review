a308919cbf479d0a3befbc77c5aa2c595059c49a
shahin-qoli <shahin-qoli@gmail.com>
2023-01-10 05:53:23 -0800

    index

--- app/serializers/spree/v2/storefront/review_serializer.rb
+++ app/serializers/spree/v2/storefront/review_serializer.rb
@@ -2,9 +2,16 @@ module Spree
   module V2
     module Storefront
       class ReviewSerializer < BaseSerializer
+        puts "FFFFFFFFFF************"
+        set_type :review
+        attributes :review, :rating, :up_vote,  :down_vote, :suggest, :summary
 
+        belongs_to :user, serializer: :user, id_method_name: :user_id, object_method_name: :user_id, record_type: :user
+        belongs_to :variant
+        #belongs_to :review_image
+        has_many   :review_conpron
 
-        puts "we are in serializer"
+        
       end
     end
   end
