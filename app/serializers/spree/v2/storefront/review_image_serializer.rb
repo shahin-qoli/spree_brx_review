f1f7971a50eb7ebf718cbb5b42bf2cedaf4497c8
shahin-qoli <shahin-qoli@gmail.com>
2023-01-10 06:35:01 -0800

    index

---
+++ app/serializers/spree/v2/storefront/review_image_serializer.rb
@@ -0,0 +1,11 @@
+module Spree
+  module V2
+    module Storefront
+      class ReviewImageSerializer < BaseSerializer
+        puts "FFFFFFFFFF************"
+        attributes :original_url
+        
+      end
+    end
+  end
+end
